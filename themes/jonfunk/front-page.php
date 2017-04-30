a3e1f90dd5ca939f30f7f67206a500701926886b
Jonathan Funk <jonfunk88@hotmail.com>
2017-04-29 19:26:06 -0700

    theme version 2 initial commit

--- themes/jonfunk/front-page.php
+++ themes/jonfunk/front-page.php
@@ -11,4 +11,6 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 		</main><!-- #main -->
 	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- themes/jonfunk/header.php
+++ themes/jonfunk/header.php
@@ -18,21 +18,18 @@
 
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
-			<header id="masthead" class="site-header" role="banner">
-				<div class="container">
-          <div class="site-branding">
-            <h1><?php bloginfo( 'name' ); ?></h1>
-            <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-          </div><!-- .site-branding -->
+			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
-          <nav id="site-navigation" class="main-navigation" role="navigation">
-             <ul>
-              <li><a href="https://ca.linkedin.com/in/jonathan-funk" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></a></i>
-              <li><a href="https://github.com/jonathanfunk" target="_blank"><i class="fa fa-github-alt" aria-hidden="true"></a></i>
-              <li><a href="mailto:jonfunk88@mail.com"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
-            </ul>
-          </nav><!-- #site-navigation -->
-        </div>
+			<header id="masthead" class="site-header" role="banner">
+				<div class="site-branding">
+					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+				</div><!-- .site-branding -->
+
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
