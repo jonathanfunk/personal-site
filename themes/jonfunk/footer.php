49dfa63278fb129f0d0290cf4e9ea54e0c766538
Jonathan Funk <jonfunk88@hotmail.com>
2017-05-10 12:07:29 -0700

    footer styled

--- themes/jonfunk/footer.php
+++ themes/jonfunk/footer.php
@@ -8,6 +8,20 @@
 ?>
 
 			</div><!-- #content -->
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="container">
+					<div class="site-info">
+						<p>Copyright &copy; <?php echo date("Y"); ?> Jonathan Funk</p>
+					</div>
+					<div class="social-icons">
+         		<ul>
+              <li><a href="https://ca.linkedin.com/in/jonathan-funk" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></a></i>
+              <li><a href="https://github.com/jonathanfunk" target="_blank"><i class="fa fa-github-alt" aria-hidden="true"></a></i>
+              <li><a href="/cdn-cgi/l/email-protection#513b3e3f37243f3a6969113c30383d7f323e3c"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
+					  </ul>						
+					</div>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
 		<?php wp_footer(); ?>
