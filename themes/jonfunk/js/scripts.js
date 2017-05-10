266ea091327c5623af70f5ec54af71ea6a25264f
Jonathan Funk <jonfunk88@hotmail.com>
2017-05-10 15:29:53 -0700

    navigation animation made

--- themes/jonfunk/js/scripts.js
+++ themes/jonfunk/js/scripts.js
@@ -1,5 +1,12 @@
 (function($) {
 
-  console.log('hello');
+ $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll > 0) {
+          $('#masthead').addClass('nav-inverse');
+      } else {
+          $('#masthead').removeClass('nav-inverse');
+      }
+    });
 
 })( jQuery );
